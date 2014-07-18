44ecc77a0199b19ea6bb71a9265ab3c6d54daa7f
PeterWuMC <peter.wu@simplybusiness.co.uk>
2014-07-18 15:34:00 +0100

    Indent with spaces

--- lib/fuelsdk/objects.rb
+++ lib/fuelsdk/objects.rb
@@ -15,54 +15,54 @@ module FuelSDK
       module CUD #create, update, delete
         def post
           if self.respond_to?('folder_property') && !self.folder_id.nil?
-			properties[self.folder_property]  = self.folder_id
-		  elsif self.respond_to?('folder_property') && !self.folder_property.nil? && !client.package_name.nil? then
-			  if client.package_folders.nil? then
-				  getPackageFolder = ET_Folder.new
-				  getPackageFolder.authStub = client
-				  getPackageFolder.properties = ["ID", "ContentType"]
-				  getPackageFolder.filter = {"Property" => "Name", "SimpleOperator" => "equals", "Value" => client.package_name}
-				  resultPackageFolder = getPackageFolder.get
-				  if resultPackageFolder.status then
-					  client.package_folders = {}
-					  resultPackageFolder.results.each do |value|
-						  client.package_folders[value[:content_type]] = value[:id]
-					  end
-				  else
-					  raise "Unable to retrieve folders from account due to: #{resultPackageFolder.message}"
-				  end
-			  end
+      properties[self.folder_property]  = self.folder_id
+      elsif self.respond_to?('folder_property') && !self.folder_property.nil? && !client.package_name.nil? then
+        if client.package_folders.nil? then
+          getPackageFolder = ET_Folder.new
+          getPackageFolder.authStub = client
+          getPackageFolder.properties = ["ID", "ContentType"]
+          getPackageFolder.filter = {"Property" => "Name", "SimpleOperator" => "equals", "Value" => client.package_name}
+          resultPackageFolder = getPackageFolder.get
+          if resultPackageFolder.status then
+            client.package_folders = {}
+            resultPackageFolder.results.each do |value|
+              client.package_folders[value[:content_type]] = value[:id]
+            end
+          else
+            raise "Unable to retrieve folders from account due to: #{resultPackageFolder.message}"
+          end
+        end
 
-			  if !client.package_folders.has_key?(self.folder_media_type) then
-				  if client.parentFolders.nil? then
-					  parentFolders = ET_Folder.new
-					  parentFolders.authStub = client
-					  parentFolders.properties = ["ID", "ContentType"]
-					  parentFolders.filter = {"Property" => "ParentFolder.ID", "SimpleOperator" => "equals", "Value" => "0"}
-					  resultParentFolders = parentFolders.get
-					  if resultParentFolders.status then
-						  client.parent_folders = {}
-						  resultParentFolders.results.each do |value|
-							  client.parent_folders[value[:content_type]] = value[:id]
-						  end
-					  else
-						  raise "Unable to retrieve folders from account due to: #{resultParentFolders.message}"
-					  end
-				  end
+        if !client.package_folders.has_key?(self.folder_media_type) then
+          if client.parentFolders.nil? then
+            parentFolders = ET_Folder.new
+            parentFolders.authStub = client
+            parentFolders.properties = ["ID", "ContentType"]
+            parentFolders.filter = {"Property" => "ParentFolder.ID", "SimpleOperator" => "equals", "Value" => "0"}
+            resultParentFolders = parentFolders.get
+            if resultParentFolders.status then
+              client.parent_folders = {}
+              resultParentFolders.results.each do |value|
+                client.parent_folders[value[:content_type]] = value[:id]
+              end
+            else
+              raise "Unable to retrieve folders from account due to: #{resultParentFolders.message}"
+            end
+          end
 
-				  newFolder = ET_Folder.new
-				  newFolder.authStub = client
-				  newFolder.properties = {"Name" => client.package_name, "Description" => client.package_name, "ContentType"=> self.folder_media_type, "IsEditable"=>"true", "ParentFolder" => {"ID" => client.parentFolders[self.folder_media_type]}}
-				  folderResult = newFolder.post
-				  if folderResult.status then
-					  client.package_folders[self.folder_media_type]  = folderResult.results[0][:new_id]
-				  else
-					  raise "Unable to create folder for Post due to: #{folderResult.message}"
-				  end
+          newFolder = ET_Folder.new
+          newFolder.authStub = client
+          newFolder.properties = {"Name" => client.package_name, "Description" => client.package_name, "ContentType"=> self.folder_media_type, "IsEditable"=>"true", "ParentFolder" => {"ID" => client.parentFolders[self.folder_media_type]}}
+          folderResult = newFolder.post
+          if folderResult.status then
+            client.package_folders[self.folder_media_type]  = folderResult.results[0][:new_id]
+          else
+            raise "Unable to create folder for Post due to: #{folderResult.message}"
+          end
 
-			  end
-			  properties[self.folder_property] = client.package_folders[self.folder_media_type]
-		  end
+        end
+        properties[self.folder_property] = client.package_folders[self.folder_media_type]
+      end
           client.soap_post id, properties
         end
 
@@ -126,19 +126,19 @@ module FuelSDK
   end
 
   class BounceEvent < Objects::Base
-	attr_accessor :get_since_last_batch
+  attr_accessor :get_since_last_batch
     include Objects::Soap::Read
   end
 
   class ClickEvent < Objects::Base
-	attr_accessor :get_since_last_batch
+  attr_accessor :get_since_last_batch
     include Objects::Soap::Read
   end
 
   class ContentArea < Objects::Base
     include Objects::Soap::Read
     include Objects::Soap::CUD
-	attr_accessor :folder_id
+  attr_accessor :folder_id
 
     def folder_property
       'CategoryID'
@@ -165,20 +165,20 @@ module FuelSDK
   class Email < Objects::Base
     include Objects::Soap::Read
     include Objects::Soap::CUD
-	attr_accessor :folder_id
+  attr_accessor :folder_id
 
-	def folder_property
-		'CategoryID'
-	end
+  def folder_property
+    'CategoryID'
+  end
 
-	def folder_media_type
-		'email'
-	end
+  def folder_media_type
+    'email'
+  end
 
     class SendDefinition < Objects::Base
       include Objects::Soap::Read
       include Objects::Soap::CUD
-	  attr_accessor :folder_id
+    attr_accessor :folder_id
 
       def id
         'EmailSendDefinition'
@@ -267,15 +267,15 @@ module FuelSDK
   class List < Objects::Base
     include Objects::Soap::Read
     include Objects::Soap::CUD
-	attr_accessor :folder_id
+  attr_accessor :folder_id
 
-	def folder_property
-		'Category'
-	end
+  def folder_property
+    'Category'
+  end
 
-	def folder_media_type
-		'list'
-	end
+  def folder_media_type
+    'list'
+  end
 
     class Subscriber < Objects::Base
       include Objects::Soap::Read
@@ -286,12 +286,12 @@ module FuelSDK
   end
 
   class OpenEvent < Objects::Base
-	attr_accessor :get_since_last_batch
+  attr_accessor :get_since_last_batch
     include Objects::Soap::Read
   end
 
   class SentEvent < Objects::Base
-	attr_accessor :get_since_last_batch
+  attr_accessor :get_since_last_batch
     include Objects::Soap::Read
   end
 
@@ -301,7 +301,7 @@ module FuelSDK
   end
 
   class UnsubEvent < Objects::Base
-	attr_accessor :get_since_last_batch
+  attr_accessor :get_since_last_batch
     include Objects::Soap::Read
   end
 
@@ -331,7 +331,7 @@ module FuelSDK
   class TriggeredSend < Objects::Base
     include Objects::Soap::Read
     include Objects::Soap::CUD
-	attr_accessor :folder_id, :subscribers
+  attr_accessor :folder_id, :subscribers
     def id
       'TriggeredSendDefinition'
     end
@@ -362,13 +362,13 @@ module FuelSDK
     include Objects::Soap::CUD
     attr_accessor :fields, :folder_id
 
-	def folder_property
-	  'CategoryID'
-	end
+  def folder_property
+    'CategoryID'
+  end
 
-	def folder_media_type
-	  'dataextension'
-	end
+  def folder_media_type
+    'dataextension'
+  end
 
     alias columns= fields= # backward compatibility
 
@@ -447,32 +447,32 @@ module FuelSDK
         #::TODO::
         # opportunity for meta programming here... but need to get this out the door
         def munge_keys d
-			if d.kind_of? Array
-			  d.each do |o|
+      if d.kind_of? Array
+        d.each do |o|
 
-				next if explicit_keys(o) && explicit_customer_key(o)
+        next if explicit_keys(o) && explicit_customer_key(o)
 
-				formatted = []
-				o['CustomerKey'] = customer_key unless explicit_customer_key o
-				unless explicit_properties(o)
-				  o.each do |k, v|
-					next if k == 'CustomerKey'
-					formatted.concat FuelSDK.format_name_value_pairs k => v
-					o.delete k
-				  end
-				  o['Keys'] = {'Key' => formatted }
-				end
-			  end
-			else
-				formatted = []
-				d.each do |k, v|
-					next if k == 'CustomerKey'
-					formatted.concat FuelSDK.format_name_value_pairs k => v
-					d.delete k
-				end
-				d['CustomerKey'] = customer_key
-				d['Keys'] = {'Key' => formatted }
-			end
+        formatted = []
+        o['CustomerKey'] = customer_key unless explicit_customer_key o
+        unless explicit_properties(o)
+          o.each do |k, v|
+          next if k == 'CustomerKey'
+          formatted.concat FuelSDK.format_name_value_pairs k => v
+          o.delete k
+          end
+          o['Keys'] = {'Key' => formatted }
+        end
+        end
+      else
+        formatted = []
+        d.each do |k, v|
+          next if k == 'CustomerKey'
+          formatted.concat FuelSDK.format_name_value_pairs k => v
+          d.delete k
+        end
+        d['CustomerKey'] = customer_key
+        d['Keys'] = {'Key' => formatted }
+      end
         end
 
         def explicit_keys h
@@ -480,30 +480,30 @@ module FuelSDK
         end
 
         def munge_properties d
-			if d.kind_of? Array
-			  d.each do |o|
-				next if explicit_properties(o) && explicit_customer_key(o)
+      if d.kind_of? Array
+        d.each do |o|
+        next if explicit_properties(o) && explicit_customer_key(o)
 
-				formatted = []
-				o['CustomerKey'] = customer_key unless explicit_customer_key o
-				unless explicit_properties(o)
-				  o.each do |k, v|
-					next if k == 'CustomerKey'
-					formatted.concat FuelSDK.format_name_value_pairs k => v
-					o.delete k
-				  end
-				  o['Properties'] = {'Property' => formatted }
-				end
-			  end
-			else
-				formatted = []
-				d.each do |k, v|
-					formatted.concat FuelSDK.format_name_value_pairs k => v
-					d.delete k
-				end
-				d['CustomerKey'] = customer_key
-				d['Properties'] = {'Property' => formatted }
-			end
+        formatted = []
+        o['CustomerKey'] = customer_key unless explicit_customer_key o
+        unless explicit_properties(o)
+          o.each do |k, v|
+          next if k == 'CustomerKey'
+          formatted.concat FuelSDK.format_name_value_pairs k => v
+          o.delete k
+          end
+          o['Properties'] = {'Property' => formatted }
+        end
+        end
+      else
+        formatted = []
+        d.each do |k, v|
+          formatted.concat FuelSDK.format_name_value_pairs k => v
+          d.delete k
+        end
+        d['CustomerKey'] = customer_key
+        d['Properties'] = {'Property' => formatted }
+      end
         end
 
         def explicit_properties h
@@ -540,31 +540,31 @@ module FuelSDK
     private
 
       def munge_fields d
-		  # maybe one day will make it smart enough to zip properties and fields if count is same?
-		  if d.kind_of? Array and d.count > 1 and (fields and !fields.empty?)
-			  # we could map the field to all DataExtensions, but lets make user be explicit.
-			  # if they are going to use fields attribute properties should
-			  # be a single DataExtension Defined in a Hash
-			  raise 'Unable to handle muliple DataExtension definitions and a field definition'
-		  end
+      # maybe one day will make it smart enough to zip properties and fields if count is same?
+      if d.kind_of? Array and d.count > 1 and (fields and !fields.empty?)
+        # we could map the field to all DataExtensions, but lets make user be explicit.
+        # if they are going to use fields attribute properties should
+        # be a single DataExtension Defined in a Hash
+        raise 'Unable to handle muliple DataExtension definitions and a field definition'
+      end
 
-		  d.each do |de|
+      d.each do |de|
 
-			  if (explicit_fields(de) and (de['columns'] || de['fields'] || has_fields)) or
-				  (de['columns'] and (de['fields'] || has_fields)) or
-				  (de['fields'] and has_fields)
-				  raise 'Fields are defined in too many ways. Please only define once.' # ahhh what, to do...
-			  end
+        if (explicit_fields(de) and (de['columns'] || de['fields'] || has_fields)) or
+          (de['columns'] and (de['fields'] || has_fields)) or
+          (de['fields'] and has_fields)
+          raise 'Fields are defined in too many ways. Please only define once.' # ahhh what, to do...
+        end
 
-			  # let users who chose, to define fields explicitly within the hash definition
-			  next if explicit_fields de
+        # let users who chose, to define fields explicitly within the hash definition
+        next if explicit_fields de
 
-			  de['Fields'] = {'Field' => de['columns'] || de['fields'] || fields}
-			  # sanitize
-			  de.delete 'columns'
-			  de.delete 'fields'
-			  raise 'DataExtension needs atleast one field.' unless de['Fields']['Field']
-		  end
+        de['Fields'] = {'Field' => de['columns'] || de['fields'] || fields}
+        # sanitize
+        de.delete 'columns'
+        de.delete 'fields'
+        raise 'DataExtension needs atleast one field.' unless de['Fields']['Field']
+      end
       end
 
       def explicit_fields h
