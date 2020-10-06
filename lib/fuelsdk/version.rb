479b696f3a1137206d7c85cb2db92fe24ee4b54c
Adam Leach <adam.leach@simplybusiness.co.uk>
2020-10-06 11:55:46 +0100

    Increment the version

--- lib/fuelsdk/version.rb
+++ lib/fuelsdk/version.rb
@@ -1,5 +1,5 @@
 module FuelSDK
-  base = "0.0.9"
+  base = "0.0.10"
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
