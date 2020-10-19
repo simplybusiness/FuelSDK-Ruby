7f5d99651fd4a4b0c0b78b087bdc487ef0e1baa9
Paweł Madejski <pawel.madejski@simplybusiness.co.uk>
2020-10-19 13:58:46 +0200

    Bump version: Fix authorization method to use generic `Authorization` header
    access_token provided in the QueryString was just an exception for
    Chopin and Coco, any other service would not be allowed to use V1 api
    with this authorization method.
    
    Header authorization does work for all V1 services.

--- lib/fuelsdk/version.rb
+++ lib/fuelsdk/version.rb
@@ -1,5 +1,5 @@
 module FuelSDK
-  base = "0.0.11"
+  base = "0.0.12"
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
